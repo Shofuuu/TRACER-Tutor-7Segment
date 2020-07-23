2edb00bafbc9193654178ef731aa7e0632fd4805
Muh. Shofuwan Anwar <44674870+Shofuuu@users.noreply.github.com>
2020-07-23 19:55:16 +0700

    Update access-seven-segment.ino

--- access-seven-segment.ino
+++ access-seven-segment.ino
@@ -17,17 +17,19 @@
 #include <Arduino.h>
 
 void setup(){
-  for(uint8_t pin=2; pin<=9; pin++)
+  for(uint8_t pin=2; pin<=9; pin++){
     pinMode(pin, OUTPUT);
-  
-  for(uint8_t pin=2; pin<=9; pin++)
-    pinMode(pin, HIGH);
+    digitalWrite(pin, HIGH);
+  }
   
   pinMode(10, OUTPUT); // enable seven segment
   digitalWrite(10, HIGH); // Turn off seven segment
 }
 
 void loop(){
+  //Turn On Seven Segment
+  digitalWrite(10,LOW);
+  
   // Send number 3 to seven segment
   digitalWrite(2, HIGH); // H
   digitalWrite(3, LOW); // G
